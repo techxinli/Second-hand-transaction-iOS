4269946777cbf79745da564ad31d5eb5a13eff41
李鑫 <lixin@lixindeMacBook-Pro.local>
2015-05-03 23:16:27 +0800

    Initial Commit

---
+++ 易兔/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  易兔
+//
+//  Created by 李鑫 on 15/5/3.
+//  Copyright (c) 2015年 Miracle Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 易兔/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  易兔
+//
+//  Created by 李鑫 on 15/5/3.
+//  Copyright (c) 2015年 Miracle Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
